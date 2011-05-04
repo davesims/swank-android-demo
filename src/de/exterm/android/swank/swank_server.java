4e23035ad4d0145aae1efcb07d28437f71d99ff4
Philip Mueller <mail@philip.in-aachen.net>
2011-05-04 17:26:28 +0200

    restarted from scratch with an auto-generated project. Used up-to-date android sdk.

---
+++ src/de/exterm/android/swank/swank_server.java
@@ -0,0 +1,15 @@
+package de.exterm.android.swank;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class swank_server extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
