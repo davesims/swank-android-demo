cab45245d9c9e6a978636f5c973ec6f88c9e81a4
Philip Mueller <mail@philip.in-aachen.net>
2011-05-09 02:27:36 +0200

    more complex example

--- src/de/exterm/android/swank.clj
+++ src/de/exterm/android/swank.clj
@@ -3,9 +3,29 @@
 	[swank.swank :as swank])
   (:import (android.widget TextView)))
 
+(defn calculate
+  [context parent-view a b]
+  (let [tv (new android.widget.TextView context)]
+    (.setText tv (str a " + " b " = " (+ a b)))
+    (.addView parent-view tv)))
+
+(defn create-main
+  [context]
+  (let [ll (new android.widget.LinearLayout context)
+	button (new android.widget.Button context)]
+    (.setOrientation ll android.widget.LinearLayout/VERTICAL)
+    (.setText button "Calculate with 7 and 6")
+    (on-click button (calculate context ll 7 6))
+    (.addView ll button)
+    (.setContentView context ll)))
+
 (defactivity Main
   (:create
-   (let [tv (new android.widget.TextView context)]
-     (.setText tv "Clojure says: Take this REPL and hack on.")
-     (.setContentView context tv))
+   (create-main context)
+   (def de.exterm.android.swank/context context)
    (swank/start-repl 4005)))
+
+(defn reload
+  [context activity]
+  (let [intent (new android.content.Intent context activity)]
+    (.startActivity context intent)))
