f8806efd0b70cc61a7089be7b602d71c48f1060d
Philip Mueller <mail@philip.in-aachen.net>
2011-05-09 16:45:06 +0200

    version used in the demo video

--- src/de/exterm/android/swank.clj
+++ src/de/exterm/android/swank.clj
@@ -1,31 +1,37 @@
 (ns de.exterm.android.swank
   (:use clj-android
 	[swank.swank :as swank])
-  (:import (android.widget TextView)))
+  (:import [android.widget TextView]))
 
-(defn calculate
-  [context parent-view a b]
+(defmacro with-ui-thread
+  "Run the given body form in the UI thread."
+  [context & body]
+  `(.runOnUiThread ~context (fn [] (do ~@body))))
+
+(defn add-log-msg
+  [context parent-view text]
   (let [tv (new android.widget.TextView context)]
-    (.setText tv (str a " + " b " = " (+ a b)))
+    (.setText tv text)
     (.addView parent-view tv)))
 
 (defn create-main
   [context]
-  (let [ll (new android.widget.LinearLayout context)
-	button (new android.widget.Button context)]
+  (let [ll (new android.widget.LinearLayout context)]
     (.setOrientation ll android.widget.LinearLayout/VERTICAL)
-    (.setText button "Calculate with 7 and 6")
-    (on-click button (calculate context ll 7 6))
-    (.addView ll button)
+    (.addView ll (create-button context
+				"Press me"
+				(fn [] (add-log-msg context ll "works."))))
     (.setContentView context ll)))
 
 (defactivity Main
   (:create
    (create-main context)
-   (def de.exterm.android.swank/context context)
+   (def de.exterm.android.swank/*context* context)
    (swank/start-repl 4005)))
 
-(defn reload
-  [context activity]
-  (let [intent (new android.content.Intent context activity)]
-    (.startActivity context intent)))
+(defn create-button
+  [context label handler]
+  (let [button (new android.widget.Button context)]
+    (.setText button label)
+    (on-click button (handler))
+    button))
